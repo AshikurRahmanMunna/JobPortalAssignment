66e44d220b511574a362bc2b2cc658d21b98c148
AshikurRahmanMunna <ashikurrahmanmunna3@gmail.com>
2022-10-10 16:29:09 +0600

    Connected to db

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
+require("colors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+// connect to MongoDB
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@jobportalassignment.eriushz.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`
+  )
+  .then(() => console.log(`Database Connected`.magenta))
+  .catch((err) => console.log(err));
+
+app.get("/", (req, res) => {
+  res.send("Job Portal Assignment server is running");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.cyan.bold);
+});
